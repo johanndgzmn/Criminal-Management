859be7ce266e317f4d5865c34cd63f63411bc635
johanndgzmn <johanndgzmn@gmail.com>
2024-01-23 20:21:52 +0800

    Transported to flutter

---
+++ android/app/src/main/kotlin/com/example/garbage_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.garbage_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+    );
+  }
+}
