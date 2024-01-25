7e9307b5bdaa6981c2c037aafa7defd84fb7e73a
N203-29 <N203-29>
2024-01-25 08:07:22 +0800

    Added live update page.

---
+++ lib/live_update.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LiveUpdate extends StatefulWidget {
+  const LiveUpdate({super.key});
+
+  @override
+  State<LiveUpdate> createState() => _LiveUpdate();
+}
+
+class _LiveUpdate extends State<LiveUpdate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
